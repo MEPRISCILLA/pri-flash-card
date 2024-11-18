4eb861f6d1e40ffff7db2d6317ac52d07a81c1b2
MEPRISCILLA <00001137225051SP@AL.EDUCACAO.SP.GOV.BR>
2024-11-18 14:01:07 -0300

    versão 1

---
+++ perguntas.js
@@ -0,0 +1,49 @@
+criaCartao(
+    "Sistema Solar", 
+    "Qual o menor planeta do Sistema Solar?",  
+    "Mercúrio é o menor planeta do Sistema Solar" 
+)
+
+criaCartao(
+    "Sistema Solar",
+    "Vênus possui satélites naturais?",
+    "Não, Vênus não tem satélites naturais"
+)
+criaCartao(
+    "Sistema Solar",
+    "Quantos satélites a Terra possui?",
+    "A Terra possui um satélite natural: Lua"
+)
+
+criaCartao(
+    "Sistema Solar",
+    'Qual planeta é conhecido como "Planeta Vermelho"?',
+    "Marte"
+    
+)
+criaCartao(
+    "Sistema Solar",
+    "Qual é o maiorplaneta do nosso sistema solar?",
+    "Júpiter é o maior"
+)
+criaCartao(
+    "sistema Solar",
+    "Qual planeta possui anéis bem visíveis?",
+    "saturno é famoso pelos seus anéis visíveis"
+
+)
+criaCartao(
+    "Sistema Solar",
+    "Qual planeta gira inclinado em seu eixo?",
+    "Urano gira quase de lado em seu eixo"
+)
+criaCartao(
+    "Sistema Solar",
+    "Qual é o planeta mais distante do Sol?",
+    "Netuno é o mais distante"
+)
+criaCartao(
+    "Sistema Solar",
+    "Plutão ainda é considerado um planeta?",
+    "Plutão foi reclassificado como planeta anãos"
+)
